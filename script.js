7329902707b0c2e1f5d82641ee91242f497d114b
toryzb <tory.zb@gmail.com>
2024-09-15 21:06:23 +0300

    edit: fix index, styles, script files 15.09.2024 21.06 Sun

---
+++ script.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+const slides2 = document.querySelector('.slides-list');
+const slideTextbox = document.querySelector('.slide-textbox');
+const sliderNavBtn = document.querySelectorAll('.slider-nav-btn');
+
+let currentSlide = 0;
+
+sliderNavBtn.forEach((control, index) => {
+  control.addEventListener('click', (event) => {
+    event.preventDefault();
+    currentSlide = index;
+
+    slides2.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+    slideTextbox.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+    sliderNavBtn.forEach((btn, btnIndex) => {
+      if (btnIndex === currentSlide) {
+        btn.setAttribute('disabled', true);
+      } else {
+        btn.removeAttribute('disabled');
+      }
+    });
+  });
+});
